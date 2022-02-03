3efb6d363f6025bd27cf7cee19068eb1e71464ad
razorsharp021 <reesarmani@gmail.com>
2022-02-03 15:14:59 +1100

    project done

--- README.md
+++ README.md
@@ -1,38 +1,28 @@
 # Thick tac toe
+
 The game is played between two players where each player gets to select a box per turn. Any player who successfully selects three boxes in a row (on any direction) wins the game.
 If neither of the players can do this, the game will be a draw.
 
-### Approach to prolem
+### Approach to problem
 
-1.First of all, we need to create the game board, which is made of **9** collective boxes, all attached together to form three columns and three rows.
-Therefore we need a main body for the board with the desired height and width and inside the board, we are going to create the individual boxes.
-Each individual box would be given a unique **ID** so it can be accessed easily through querySelector.
+1.First of all, we need to create the game board, 3 rows and 3 columns
+2.create a main container and put the boxes insideinside the board
+3.give each box a unique **ID**
+4.We have two players, so we create ( **'X'** and **'O'** ) to toggle between the two
+5.Each player gets to choose an empty slot per turn it has to be an empty available box only...
 
-2.We have two players,therefore we need two deifferent choices ( **'X'** and **'O'** ) to be assigned to player one and player two.
-Each player gets to choose an empty slot per turn,which can be achieved through addEventListener method,and has to be an empty available box only...
-When player selects the box, it will now be the next players turn, and the choice would turn into **'X'** or **'O'**.
-
-3.We need to assign an array of choices for each player that contains the **ID** of selected boxes.
-Each time a player selects a box, the id would get **pushed** into the array.
-We also need to assign an array, containing the **ID** of the boxes with winning condition.
-
-4.Players are to be asked to make a valid choice when click the wrong slot (*e.g.* an already selected one)
-If any player clicks an invalid box, the game should not go on until they make a correct selection (*e.g.* player one clicks a used box the choice should stay **'X'** an player one still making a choice )
-
-5.The players choice is to be toggled between **'X'** and **'O'** after each valid choice, for the next player to play the game.
-
-6.**Game Logic:** if there is no winner (No consecutive 3 boxes selected by any players) the players won't get any score, and the game will restart.
-If there is a winner (either of the playes get to choose three consecutive **'X'** or **'O'**) then we have a winner which will be announced, and the winner gets a score.
-We need to program a loop to compare the possible winning condition array with each of the players choices array to achieve this.
-
+6.**Game Logic:** if there is no winner (No consecutive 3 boxes selected by any players) result is draw and game restarts.
+If there is a winner, will be announced, and the winner gets a score.
 
 ### Cool tech used
+
 images etc
 fonts
 
 ### Lessons you learnt doing the project
+
 importance of planning
 
 ### Future features
-keep a scoreboard
 
+keep a scoreboard
--- index.js
+++ index.js
@@ -1,19 +1,173 @@
-// TIC TAC TOE
+var box1 = document.querySelector("#box1");
+var box2 = document.querySelector("#box2");
+var box3 = document.querySelector("#box3");
+var box4 = document.querySelector("#box4");
+var box5 = document.querySelector("#box5");
+var box6 = document.querySelector("#box6");
+var box7 = document.querySelector("#box7");
+var box8 = document.querySelector("#box8");
+var box9 = document.querySelector("#box9");
+var clickNum = 0;
+var win = false;
+var gameStat = document.querySelector("h2");
+var resButton = document.querySelector(".restart-btn");
 
-// 1/first we need the game board,which can be made of 9 collective boxes, possibly divided into three vertical columns and three horizental rows.
-// So we will create a main body for the board with the desired height and width and the inside the board, we are going to create the individual boxes, which can be put into different sections or divisions if needed to
+function mark(event) {
+  var boxClicked = event.target;
+  if (boxClicked.textContent === "" && clickNum % 2 === 0) {
+    boxClicked.textContent = "X";
+    clickNum++;
+    if (
+      box1.textContent === "X" &&
+      box2.textContent === "X" &&
+      box3.textContent === "X"
+    ) {
+      win = true;
+      gameStat.textContent = "Player One Wins!";
+      playerChoice.removeEventListener("click", mark);
+    } else if (
+      box1.textContent === "X" &&
+      box4.textContent === "X" &&
+      box7.textContent === "X"
+    ) {
+      win = true;
 
-// 2/Each box would be given a unique ID so it can be accessed easily through querySelector.
+      gameStat.textContent = "Player One Wins!";
+      playerChoice.removeEventListener("click", mark);
+    } else if (
+      box1.textContent === "X" &&
+      box5.textContent === "X" &&
+      box9.textContent === "X"
+    ) {
+      win = true;
+      gameStat.textContent = "Player One Wins!";
+      playerChoice.removeEventListener("click", mark);
+    } else if (
+      box2.textContent === "X" &&
+      box5.textContent === "X" &&
+      box8.textContent === "X"
+    ) {
+      win = true;
+      gameStat.textContent = "Player One Wins!";
+      playerChoice.removeEventListener("click", mark);
+    } else if (
+      box3.textContent === "X" &&
+      box6.textContent === "X" &&
+      box9.textContent === "X"
+    ) {
+      win = true;
+      gameStat.textContent = "Player One Wins!";
+      playerChoice.removeEventListener("click", mark);
+    } else if (
+      box3.textContent === "X" &&
+      box5.textContent === "X" &&
+      box7.textContent === "X"
+    ) {
+      win = true;
+      gameStat.textContent = "Player One Wins!";
+      playerChoice.removeEventListener("click", mark);
+    } else if (
+      box4.textContent === "X" &&
+      box5.textContent === "X" &&
+      box6.textContent === "X"
+    ) {
+      win = true;
+      gameStat.textContent = "Player One Wins!";
+      playerChoice.removeEventListener("click", mark);
+    } else if (
+      box7.textContent === "X" &&
+      box8.textContent === "X" &&
+      box9.textContent === "X"
+    ) {
+      win = true;
+      gameStat.textContent = "Player One Wins!";
+      playerChoice.removeEventListener("click", mark);
+    } else if (clickNum === 9 && win === false) {
+      gameStat.textContent = "!DRAW!";
+      playerChoice.removeEventListener("click", mark);
+    }
+  } else {
+    if (boxClicked.textContent === "") {
+      boxClicked.textContent = "O";
+      clickNum++;
+      if (
+        box1.textContent === "O" &&
+        box2.textContent === "O" &&
+        box3.textContent === "O"
+      ) {
+        win = true;
+        gameStat.textContent = "Player Two Wins!";
+        playerChoice.removeEventListener("click", mark);
+      } else if (
+        box1.textContent === "O" &&
+        box4.textContent === "O" &&
+        box7.textContent === "O"
+      ) {
+        win = true;
+        gameStat.textContent = "Player Two Wins!";
+        playerChoice.removeEventListener("click", mark);
+      } else if (
+        box1.textContent === "O" &&
+        box5.textContent === "O" &&
+        box9.textContent === "O"
+      ) {
+        win = true;
+        gameStat.textContent = "Player Two Wins!";
+        playerChoice.removeEventListener("click", mark);
+      } else if (
+        box2.textContent === "O" &&
+        box5.textContent === "O" &&
+        box8.textContent === "O"
+      ) {
+        win = true;
+        gameStat.textContent = "Player Two Wins!";
+        playerChoice.removeEventListener("click", mark);
+      } else if (
+        box3.textContent === "O" &&
+        box6.textContent === "O" &&
+        box9.textContent === "O"
+      ) {
+        win = true;
+        gameStat.textContent = "Player Two Wins!";
+        playerChoice.removeEventListener("click", mark);
+      } else if (
+        box3.textContent === "O" &&
+        box5.textContent === "O" &&
+        box7.textContent === "O"
+      ) {
+        win = true;
+        gameStat.textContent = "Player Two Wins!";
+        playerChoice.removeEventListener("click", mark);
+      } else if (
+        box4.textContent === "O" &&
+        box5.textContent === "O" &&
+        box6.textContent === "O"
+      ) {
+        win = true;
+        gameStat.textContent = "Player Two Wins!";
+        playerChoice.removeEventListener("click", mark);
+      } else if (
+        box7.textContent === "O" &&
+        box8.textContent === "O" &&
+        box9.textContent === "O"
+      ) {
+        win = true;
+        gameStat.textContent = "Player Two Wins!";
+        playerChoice.removeEventListener("click", mark);
+      } else if (clickNum === 9 && win === false) {
+        gameStat.textContent = "DRAW!";
+        playerChoice.removeEventListener("click", mark);
+      }
+    }
+  }
+}
 
-// 3/We have two players,therefore we need two deifferent choices ( X and O) to be assigned to player one and player two.
+var playerChoice = document.querySelector(".game-board");
+playerChoice.addEventListener("click", mark);
 
-// 4/Each player gets to choose an empty slot per turn,which can be achieved through addEventListener method,and has to be an empty available box only...
-// When player selects the box, it will turn into X or O depending on which player did the selection
-
-// 5/Players are to be asked to make a valid choice when click the wrong slot (e.g. an already selected one)
-// If any player clicks an invalid box, the game should not go on until they make a correct selection (If player one clicks a used box the choice should stay X an player one still making a choice )
-
-// 6/The players choice is to be toggled between 'X' and 'O' after each valid choice, for the next player to play the game.
-
-// 7/Game Logic : if there is no winner (No consecutive 3 boxes selected by any players) the players won't get any score, and the game will restart.
-// If there is a winner (either of the playes: three consecutive X's or O's are selected by a player) then we have a winner which will be announced, and the winner gets a score.
+resButton.addEventListener("click", function () {
+  window.location.reload();
+});
+// window.onload = function () {
+//   document.getElementById("#sound-track").play();
+// };
